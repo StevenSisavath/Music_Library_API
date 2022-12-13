a59d0311a7326fa6932971fc69b4e530988f8fe0
Steven Sisavath <stevenlsisavath@gmail.com>
2022-12-13 02:24:49 -0600

    Added ERD

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
 
 @api_view(['GET'])
 def songs_list(request):
